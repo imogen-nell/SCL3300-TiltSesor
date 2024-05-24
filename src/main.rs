e6fd106dfed428043141ecacc25a4bb537848c4e
Javier Romualdez <javier@starspectech.com>
2024-05-24 15:59:24 -0400

    --

--- src/main.rs
+++ src/main.rs
@@ -281,6 +281,8 @@ fn execute_angles(spi: &mut Spidev, cs: &mut OutputPin){
 
 fn anlge_conversion(data: Vec<u8>) -> f64 {
     let abs_val = i16::from_le_bytes([data[0], data[1]]) as f64;
+    println!("signed value: {}", abs_val);
+    println!("unsigned value: {}", u16::from_le_bytes([data[0], data[1]]) as f64);
     let angle = (((abs_val.abs() / 2_i16.pow(14) as f64) * 90.0) * 100.0).round() / 100.0;
     angle
 }
