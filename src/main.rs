a79452bc4c0da40ef1c321837b73c54e3d92755b
Javier Romualdez <javier@starspectech.com>
2024-05-24 11:44:31 -0400

    hex reads

--- src/main.rs
+++ src/main.rs
@@ -129,7 +129,7 @@ fn read(spi: &mut Spidev, cs: &mut OutputPin) -> Result<Vec<u8>, Box<dyn Error>>
     std::thread::sleep(std::time::Duration::from_millis(20));
     cs.set_high();
     std::thread::sleep(std::time::Duration::from_millis(15));
-    println!("read: {:?}", &ret);
+    println!("read: {:?}", ret.iter().map(|x| format!("{:02X}", x)).collect::<Vec<_>>());
     Ok(ret) // Return the owned Vec<u8>
 }
 
